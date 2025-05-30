bcb934cd186d4aef1e48d307e803dfb3af636fa0
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2025-05-30 22:01:56 +0200

    split product dto

---
+++ ShopAPI/Dtos/Product/ReadProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopAPI.Dtos.Product;
+
+public class ReadProductDto
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public decimal Price { get; set; }
+    public int CategoryId { get; set; }
+}
---
+++ ShopAPI/Dtos/Product/WriteProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopAPI.Dtos.Product;
+
+public class WriteProductDto
+{
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public decimal Price { get; set; }
+    public int CategoryId { get; set; }
+}
