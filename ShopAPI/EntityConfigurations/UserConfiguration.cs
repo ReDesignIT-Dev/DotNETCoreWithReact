99d363820c10e0e4ea040b1375d3b707d54b80b8
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2025-06-01 00:18:52 +0200

    add user configuration

---
+++ ShopAPI/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShopAPI.Models;
+
+namespace ShopAPI.EntityConfigurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        // Username: required, unique, max length 64
+        builder.Property(u => u.Username)
+            .IsRequired()
+            .HasMaxLength(64);
+
+        builder.HasIndex(u => u.Username)
+            .IsUnique();
+
+        // PasswordHash and PasswordSalt: required
+        builder.Property(u => u.PasswordHash)
+            .IsRequired();
+
+        builder.Property(u => u.PasswordSalt)
+            .IsRequired();
+    }
+}
