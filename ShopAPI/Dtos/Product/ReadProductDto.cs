e26c5825a456ffb8edacd099ee1e9127033791b3
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2025-06-03 18:15:24 +0200

    add images to dto

--- ShopAPI/Dtos/Product/ReadProductDto.cs
+++ ShopAPI/Dtos/Product/ReadProductDto.cs
@@ -7,4 +7,5 @@ public class ReadProductDto
     public required string Description { get; set; }
     public decimal Price { get; set; }
     public int CategoryId { get; set; }
+    public List<ProductImageDto> Images { get; set; } = new();
 }
