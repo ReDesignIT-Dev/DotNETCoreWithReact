48a2dbb4fcc92bd8861cc122866942fbd04d9460
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2025-06-04 22:18:51 +0200

    update model to not require product, it will be handled by service

--- ShopAPI/Models/ProductImage.cs
+++ ShopAPI/Models/ProductImage.cs
@@ -5,5 +5,5 @@ public class ProductImage
     public int Id { get; set; }
     public required string Url { get; set; }
     public int ProductId { get; set; }
-    public required Product Product { get; set; }
+    public Product Product { get; set; } = null!;
 }
