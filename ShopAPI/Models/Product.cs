e4444e23835f35c9a502c1418ab3d803c2e809bd
Arkadiusz <arkadiusz.budkowski.dev@gmail.com>
2025-06-08 10:48:37 +0200

    Add `Sku` property to `Product` class for inventory tracking
    Added a new `Sku` property to the `Product` class in the
    `ShopAPI.Models` namespace. The property is a `string`
    initialized to `String.Empty` and represents the Stock
    Keeping Unit (SKU), commonly used for product identification
    and inventory management.

--- ShopAPI/Models/Product.cs
+++ ShopAPI/Models/Product.cs
@@ -7,6 +7,7 @@ public class Product
     public int Id { get; set; }
     public required string Name { get; set; }
     public string Slug { get; set; } = String.Empty;
+    public string Sku { get; set; } = String.Empty; // STOCK KEEPING UNIT
     public required string Description { get; set; }
     public required decimal Price { get; set; }
     public int CategoryId { get; set; }
